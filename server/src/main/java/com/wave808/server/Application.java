69f8461070827e29ac3305d6009f4b3f2bd505c0
ivan.barra <ivan.barra@itecan.es>
2026-04-09 13:36:19 +0200

    Creado proyecto cliente con javafx en maeven hecho

--- server/src/main/java/com/wave808/server/Application.java
+++ server/src/main/java/com/wave808/server/Application.java
@@ -1,11 +1,7 @@
 package com.wave808.server;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.wave808.server.services.UserService;
 
 @SpringBootApplication
 public class Application {
@@ -17,14 +13,13 @@ public class Application {
 		
 	}
 	
-	@Bean
-	//@SuppressWarnings("unused") 
-	CommandLineRunner initDatabase(UserService userService) {
-    	return args -> {
-        userService.registerUser("admin", "admin@808wave.com", "admin");
-        System.out.println("Admin creado");
-    	};
-	}
+	// @Bean
+	// CommandLineRunner initDatabase(UserService userService) {
+    // 	return args -> {
+    //     userService.registerUser("admin", "admin@808wave.com", "admin");
+    //     System.out.println("Admin creado");
+    // 	};
+	// }
 
 
 }
