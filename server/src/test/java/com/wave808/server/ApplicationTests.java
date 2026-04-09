a1c972e7edf678e7be9d047751132e83796b76ad
ivan.barra <ivan.barra@itecan.es>
2026-04-09 09:59:13 +0200

    chore: setup inicial del workspace, spring boot y db docker

---
+++ server/src/test/java/com/wave808/server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wave808.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
